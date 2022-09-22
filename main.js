b4d521077681a46e41ef5c40dc6fa3a84f071999
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-09-22 17:51:24 -0500

    Working on menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const burger = document.querySelector('.menu');
+const mobilemenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burger.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobilemenu.classList.toggle('inactive');
 }
