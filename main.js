23844135a3d8ee4326e8df1b2f9a4fd4262ea549
AlejandroChing11 <alejandroching2004@hotmail.com>
2022-09-23 14:32:17 -0500

    Shopping car ready

--- main.js
+++ main.js
@@ -2,14 +2,39 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const burger = document.querySelector('.menu');
 const mobilemenu = document.querySelector('.mobile-menu');
+const carrito = document.querySelector('.navbar-shopping-cart');
+const pdetails = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burger.addEventListener('click', toggleMobileMenu);
+carrito.addEventListener('click', toggleCarrito);
 
 function toggleDesktopMenu() {
+    const ispdetailsclosed = pdetails.classList.contains('inactive');
+    if (!ispdetailsclosed) {
+        pdetails.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu() {
+    const ispdetailsclosed = pdetails.classList.contains('inactive');
+    if(!ispdetailsclosed) {
+        pdetails.classList.add('inactive');
+    }
     mobilemenu.classList.toggle('inactive');
+}
+
+function toggleCarrito() {
+    const ismobilemenuopen = mobilemenu.classList.contains('inactive');
+    
+    if(!ismobilemenuopen) {
+        mobilemenu.classList.add('inactive');
+    }
+
+    if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive');
+    }
+
+    pdetails.classList.toggle('inactive');
 }
